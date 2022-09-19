1141ea9a3e5e2094d50ff927d8bdf72fc42941b5
manirabona-programer <hseal419@gmail.com>
2022-09-19 13:05:34 +0200

    dev: added bootstrap - added header, hero and navbar for global component

---
+++ resources/views/components/home/health-care.blade.php
@@ -0,0 +1,13 @@
+<div id="services" class="services section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+            <h4 class="text-4xl">We put Care into Healthcare</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <div class="header-readme-button"><a href="#">Request a Demo</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,36 @@
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+                <x-logo />
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Why vitaway</a></li>
+              <li class="scroll-to-section"><a href="#services">Who we are</a></li>
+              <li class="scroll-to-section"><a href="#about">For indivitual</a></li>
+              <li class="scroll-to-section"><a href="#pricing">For Providers</a></li>
+              <li class="scroll-to-section"><a href="#newsletter">Resource Center</a></li>
+              <li class="scroll-to-section"><a href="#newsletter">About Vitaway</a></li>
+              <li><a href="#newsletter">Member Login</a></li>
+              <li>
+                <div class="account-content">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGltZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt=""></a>
+                  </div>
+              </li>
+            </ul>
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
